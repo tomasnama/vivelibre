4ed792e0789c4ee8e04bb437546e0627c54588c4
tomasnavarro <tnavarro@atsistemas.com>
2023-02-09 11:58:40 +0100

    Organize imports

--- src/main/java/com/tomasapp/vivelibre/book/component/BookComponent.java
+++ src/main/java/com/tomasapp/vivelibre/book/component/BookComponent.java
@@ -3,7 +3,6 @@ package com.tomasapp.vivelibre.book.component;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
